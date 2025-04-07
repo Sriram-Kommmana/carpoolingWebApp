631f97119e654fff66795707513e17e68bb7466e
Sriram-Kommmana <kommanamarirs@gmail.com>
2025-04-07 22:59:26 +0530

    initial setup

---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
